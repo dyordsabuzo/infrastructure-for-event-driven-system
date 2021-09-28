642f440c1f43a667e028d1bc4e966f18ebe239a6
George Abuzo <dyordsabuzo@gmail.com>
2021-09-29 09:30:10 +1000

    Refactor for prod readiness

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
-  }
-
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "pablosspot"
