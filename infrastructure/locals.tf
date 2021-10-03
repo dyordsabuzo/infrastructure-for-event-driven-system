9072f38ad5116f9af57e0dde7900a237ae14959d
dyordsabuzo <dyordsabuzo@gmail.com>
2021-10-03 14:19:48 +1100

    Setup ci/cd for app and infra (#7)
    * Setup ci/cd for app and infra
    
    * Fix docker image vuln scan

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -5,4 +5,10 @@ locals {
 
   aws_ecr_url           = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
   docker_run_config_sha = sha256(local_file.docker_run_config.content)
+  backend_image_tag = try([
+    for t in data.aws_ecr_image.image["backend"].image_tags : t if t != "latest"
+  ][0], "latest")
+  worker_image_tag = try([
+    for t in data.aws_ecr_image.image["worker"].image_tags : t if t != "latest"
+  ][0], "latest")
 }
